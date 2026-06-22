a2ec900f64c6423fc3abd32f62a28d9e6545f277
GLA7479 <18ERAN@GMAIL.COM>
2026-06-22 21:02:41 +0300

    Auto update - 22/06/2026 21:02:41.05

--- scripts/generate-teacher-icons.ps1
+++ scripts/generate-teacher-icons.ps1
@@ -1,14 +1,86 @@
 Add-Type -AssemblyName System.Drawing
 
-function New-TeacherIcon {
+# Clone parent-icons → teacher-icons: same shield, teacher gradient, P → T.
+
+function Test-WhitePixel {
+    param([System.Drawing.Color]$Color)
+    return $Color.R -gt 235 -and $Color.G -gt 235 -and $Color.B -gt 235
+}
+
+function New-ShieldPath {
     param([int]$Size)
 
-    $bmp = New-Object System.Drawing.Bitmap $Size, $Size
-    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $shieldW = [single]($Size * 0.367)
+    $shieldH = [single]($Size * 0.359)
+    $shieldX = [single](($Size - $shieldW) / 2)
+    $shieldY = [single]($Size * 0.547)
+    $topInset = $shieldW * 0.16
+    $shoulderY = $shieldY + ($shieldH * 0.1)
+    $bottomY = $shieldY + $shieldH
+
+    $shield = New-Object System.Drawing.Drawing2D.GraphicsPath
+    $shield.AddLine(($shieldX + $topInset), $shieldY, ($shieldX + $shieldW - $topInset), $shieldY)
+    $shield.AddBezier(
+        ($shieldX + $shieldW - $topInset), $shieldY,
+        ($shieldX + $shieldW + ($shieldW * 0.035)), $shoulderY,
+        ($shieldX + $shieldW + ($shieldW * 0.035)), ($bottomY - ($shieldH * 0.16)),
+        ($shieldX + ($shieldW / 2)), $bottomY
+    )
+    $shield.AddBezier(
+        ($shieldX + ($shieldW / 2)), $bottomY,
+        ($shieldX - ($shieldW * 0.035)), ($bottomY - ($shieldH * 0.16)),
+        ($shieldX - ($shieldW * 0.035)), $shoulderY,
+        ($shieldX + $topInset), $shieldY
+    )
+    return $shield
+}
+
+function New-LetterPMask {
+    param(
+        [int]$Size,
+        [System.Drawing.Font]$Font,
+        [single]$LetterY
+    )
+
+    $mask = New-Object System.Drawing.Bitmap $Size, $Size
+    $g = [System.Drawing.Graphics]::FromImage($mask)
+    $g.Clear([System.Drawing.Color]::FromArgb(0, 0, 0, 0))
+    $g.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
+
+    $sf = New-Object System.Drawing.StringFormat
+    $sf.Alignment = [System.Drawing.StringAlignment]::Center
+    $sf.LineAlignment = [System.Drawing.StringAlignment]::Center
+    $g.DrawString("P", $Font, [System.Drawing.Brushes]::White, ($Size / 2), $LetterY, $sf)
+    $g.Dispose()
+    return $mask
+}
+
+function Get-TeacherGradientColor {
+    param([int]$X, [int]$Y, [int]$Size)
+
+    $t = ($X + $Y) / (2.0 * [Math]::Max(1, ($Size - 1)))
+    if ($t -lt 0) { $t = 0 }
+    if ($t -gt 1) { $t = 1 }
+
+    $r = [int](55 + ((234 - 55) * $t))
+    $g = [int](48 + ((88 - 48) * $t))
+    $b = [int](163 + ((12 - 163) * $t))
+    return [System.Drawing.Color]::FromArgb(255, $r, $g, $b)
+}
+
+function New-TeacherIconFromParent {
+    param([int]$Size)
+
+    $parentPath = Join-Path $PSScriptRoot "..\public\images\parent-icons\icon-$Size.png"
+    if (-not (Test-Path $parentPath)) {
+        throw "Missing parent icon: $parentPath"
+    }
+
+    $parent = [System.Drawing.Bitmap]::FromFile($parentPath)
+    $out = New-Object System.Drawing.Bitmap $Size, $Size
+    $g = [System.Drawing.Graphics]::FromImage($out)
     $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
     $g.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
-    $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
-    $g.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
 
     $rect = New-Object System.Drawing.Rectangle 0, 0, $Size, $Size
     $bgBrush = New-Object System.Drawing.Drawing2D.LinearGradientBrush $rect,
@@ -16,107 +88,56 @@ function New-TeacherIcon {
         ([System.Drawing.Color]::FromArgb(255, 234, 88, 12)),
         45
     $g.FillRectangle($bgBrush, $rect)
+    $g.Dispose()
 
-    $softGlow = New-Object System.Drawing.Drawing2D.GraphicsPath
-    $glowSize = [int]($Size * 0.72)
-    $glowX = [int](($Size - $glowSize) / 2)
-    $glowY = [int]($Size * 0.08)
-    $softGlow.AddEllipse($glowX, $glowY, $glowSize, [int]($glowSize * 0.55))
-    $glowBrush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(28, 255, 255, 255))
-    $g.FillPath($glowBrush, $softGlow)
+    $fontSize = [single]($Size * 0.41)
+    $letterY = [single]($Size * 0.285)
+    $font = New-Object System.Drawing.Font "Arial", $fontSize, ([System.Drawing.FontStyle]::Bold), ([System.Drawing.GraphicsUnit]::Pixel)
+    $pMask = New-LetterPMask -Size $Size -Font $font -LetterY $letterY
+    $shieldPath = New-ShieldPath -Size $Size
 
-    $fontSize = [single]($Size * 0.54)
-    $font = New-Object System.Drawing.Font Arial, $fontSize, ([System.Drawing.FontStyle]::Bold), ([System.Drawing.GraphicsUnit]::Pixel)
+    for ($y = 0; $y -lt $Size; $y++) {
+        for ($x = 0; $x -lt $Size; $x++) {
+            if (-not $shieldPath.IsVisible($x, $y)) { continue }
+            if (-not (Test-WhitePixel ($parent.GetPixel($x, $y)))) { continue }
+            $out.SetPixel($x, $y, [System.Drawing.Color]::White)
+        }
+    }
+
+    for ($y = 0; $y -lt $Size; $y++) {
+        for ($x = 0; $x -lt $Size; $x++) {
+            $pMaskPx = $pMask.GetPixel($x, $y)
+            if ($pMaskPx.A -gt 0 -and (Test-WhitePixel $pMaskPx)) {
+                $out.SetPixel($x, $y, (Get-TeacherGradientColor -X $x -Y $y -Size $Size))
+            }
+        }
+    }
+
+    $shieldPath.Dispose()
+    $g = [System.Drawing.Graphics]::FromImage($out)
+    $g.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
     $sf = New-Object System.Drawing.StringFormat
     $sf.Alignment = [System.Drawing.StringAlignment]::Center
     $sf.LineAlignment = [System.Drawing.StringAlignment]::Center
-    $tY = $Size * 0.36
-    $g.DrawString("T", $font, [System.Drawing.Brushes]::White, ($Size / 2), $tY, $sf)
-
-    $shieldW = [int]($Size * 0.42)
-    $shieldH = [int]($Size * 0.46)
-    $shieldX = [int](($Size - $shieldW) / 2)
-    $shieldY = [int]($Size * 0.54)
-
-    $shield = New-Object System.Drawing.Drawing2D.GraphicsPath
-    $topInset = [int]($shieldW * 0.18)
-    $shoulderY = $shieldY + [int]($shieldH * 0.12)
-    $bottomY = $shieldY + $shieldH
-    $shield.AddLine($shieldX + $topInset, $shieldY, $shieldX + $shieldW - $topInset, $shieldY)
-    $shield.AddBezier(
-        $shieldX + $shieldW - $topInset, $shieldY,
-        $shieldX + $shieldW + [int]($shieldW * 0.04), $shoulderY,
-        $shieldX + $shieldW + [int]($shieldW * 0.04), $bottomY - [int]($shieldH * 0.18),
-        $shieldX + ($shieldW / 2), $bottomY
-    )
-    $shield.AddBezier(
-        $shieldX + ($shieldW / 2), $bottomY,
-        $shieldX - [int]($shieldW * 0.04), $bottomY - [int]($shieldH * 0.18),
-        $shieldX - [int]($shieldW * 0.04), $shoulderY,
-        $shieldX + $topInset, $shieldY
-    )
-    $g.FillPath([System.Drawing.Brushes]::White, $shield)
-
-    $boardW = [int]($shieldW * 0.62)
-    $boardH = [int]($shieldH * 0.58)
-    $boardX = [int](($Size - $boardW) / 2)
-    $boardY = $shieldY + [int]($shieldH * 0.2)
-    $boardRect = New-Object System.Drawing.RectangleF $boardX, $boardY, $boardW, $boardH
-    $boardPath = New-Object System.Drawing.Drawing2D.GraphicsPath
-    $radius = [single]($boardW * 0.08)
-    $boardPath.AddArc($boardX, $boardY, $radius * 2, $radius * 2, 180, 90)
-    $boardPath.AddArc($boardX + $boardW - $radius * 2, $boardY, $radius * 2, $radius * 2, 270, 90)
-    $boardPath.AddArc($boardX + $boardW - $radius * 2, $boardY + $boardH - $radius * 2, $radius * 2, $radius * 2, 0, 90)
-    $boardPath.AddArc($boardX, $boardY + $boardH - $radius * 2, $radius * 2, $radius * 2, 90, 90)
-    $boardPath.CloseFigure()
-    $boardFill = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(255, 67, 56, 202))
-    $g.FillPath($boardFill, $boardPath)
-
-    $clipW = [int]($boardW * 0.34)
-    $clipH = [int]($boardH * 0.16)
-    $clipX = [int](($Size - $clipW) / 2)
-    $clipY = $boardY - [int]($clipH * 0.55)
-    $clipRect = New-Object System.Drawing.Rectangle $clipX, $clipY, $clipW, $clipH
-    $g.FillRectangle([System.Drawing.Brushes]::White, $clipRect)
-    $clipInner = New-Object System.Drawing.Rectangle ($clipX + [int]($clipW * 0.18)), ($clipY + [int]($clipH * 0.22)), ([int]($clipW * 0.64)), ([int]($clipH * 0.56))
-    $g.FillRectangle($boardFill, $clipInner)
-
-    $linePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(255, 255, 255, 255)), ([single]($Size * 0.018))
-    $lineStartX = $boardX + [int]($boardW * 0.16)
-    $lineEndX = $boardX + $boardW - [int]($boardW * 0.16)
-    $lineY1 = $boardY + [int]($boardH * 0.34)
-    $lineY2 = $boardY + [int]($boardH * 0.52)
-    $lineY3 = $boardY + [int]($boardH * 0.70)
-    $g.DrawLine($linePen, $lineStartX, $lineY1, $lineEndX, $lineY1)
-    $g.DrawLine($linePen, $lineStartX, $lineY2, $lineEndX, $lineY2)
-    $g.DrawLine($linePen, $lineStartX, $lineY3, [int]($lineEndX - $boardW * 0.18), $lineY3)
-
-    $tickPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(255, 251, 191, 36)), ([single]($Size * 0.022))
-    $tickPen.StartCap = [System.Drawing.Drawing2D.LineCap]::Round
-    $tickPen.EndCap = [System.Drawing.Drawing2D.LineCap]::Round
-    $tickX = $lineStartX - [int]($boardW * 0.02)
-    $g.DrawLine($tickPen, ($tickX - [int]($boardW * 0.06)), ($lineY1 + [int]($boardH * 0.02)), $tickX, ($lineY1 + [int]($boardH * 0.08)))
-    $g.DrawLine($tickPen, $tickX, ($lineY1 + [int]($boardH * 0.08)), ($tickX + [int]($boardW * 0.12)), ($lineY1 - [int]($boardH * 0.06)))
+    $g.DrawString("T", $font, [System.Drawing.Brushes]::White, ($Size / 2), $letterY, $sf)
 
     $outDir = Join-Path $PSScriptRoot "..\public\images\teacher-icons"
     if (-not (Test-Path $outDir)) {
         New-Item -ItemType Directory -Path $outDir -Force | Out-Null
     }
     $outPath = Join-Path $outDir "icon-$Size.png"
-    $bmp.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    $out.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
 
     $font.Dispose()
     $bgBrush.Dispose()
-    $glowBrush.Dispose()
-    $boardFill.Dispose()
-    $linePen.Dispose()
-    $tickPen.Dispose()
+    $pMask.Dispose()
+    $parent.Dispose()
     $g.Dispose()
-    $bmp.Dispose()
+    $out.Dispose()
 
     Write-Output "Saved $outPath"
 }
 
 foreach ($size in @(192, 512)) {
-    New-TeacherIcon -Size $size
+    New-TeacherIconFromParent -Size $size
 }
